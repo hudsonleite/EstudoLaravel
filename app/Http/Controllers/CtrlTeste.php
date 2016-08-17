dd25dc60806e05014b1f85a1a4aec387e21ef1ba
Hudson Leite <hudson_gomes1@hotmail.com>
2016-08-16 21:16:58 -0400

    Arquivos Iniciais
    Arquivos Iniciais

---
+++ app/Http/Controllers/CtrlTeste.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class CtrlTeste extends Controller
+{
+    //
+    public function index()
+    {
+    	# code...
+    	Return "Pagina Teste 02";
+    }
+
+    public function home()
+    {
+    	# code...
+    	Return "Pagina Inicial";
+    }
+
+    public function msg($value)
+    {
+    	# code...
+    	Return "Mensagem --->".$value;
+    }
+}
